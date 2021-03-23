5855d2c0a24fb93bad656963b98c8874c996687c
maribels_are_ringing <maribel.duron86@gmail.com>
2021-03-22 21:50:33 -0700

    added code to run through an array

---
+++ app.js
@@ -0,0 +1,18 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    profileDataArr.forEach(profileItem => console.log(profileItem)); {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+  };
+  
+  printProfileData(profileDataArgs);
