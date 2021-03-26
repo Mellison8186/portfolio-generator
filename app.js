80bd067fe21f3aeea2d6c04c54f999701aabacd8
maribels_are_ringing <maribel.duron86@gmail.com>
2021-03-25 22:51:34 -0700

    added input functions to the file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -127,16 +129,12 @@ if (!portfolioData.projects) {
 
 promptUser()
   .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
+  .then(mockData => {
+    const pageHTML = generatePage(mockData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
   });
-// const fs = require("fs");
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
